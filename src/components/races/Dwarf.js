f39ee6f1c1d35397fb70ebf8f866dc1fab9a30d9
Michael Herrera <michael.aaron.herrera@gmail.com>
2022-11-28 17:34:50 -0500

    Initial commit

---
+++ src/components/races/Dwarf.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import {useState} from 'react';
+import Picture from '../pics/dwarf.png'
+import {Constitution, Strength, Wisdom} from '../keywords/AbilityScores';
+
+function DwarfRace() {
+    
+    const [buttons, setButtons] = useState([
+        { class:"buttonsubrace", label: "Hill Dwarf", value: false },
+        { class:"buttonsubrace", label: "Mountain Dwarf", value: false }
+
+    ]);
+
+    const handleButtonsChange = ({
+        buttons,
+        setButtons,
+        handleButtonsChange
+    }) => label => {
+        const newButtonsState = buttons.map(button => {
+            if (button.label === label) {
+                return (button = { class: "buttonsubrace_clicked", label: button.label, value: true});
+            }
+            return {
+                class: "buttonsubrace",
+                label: button.label,
+                value: false
+            };
+        });
+        setButtons(newButtonsState)
+    };
+       
+    const Specialbuton = ({ buttons, setButtons, handleButtonsChange }) => {
+        return (
+            <>
+                {buttons.map((button,index) => (
+                    <button class={`${button.class}`}
+                        key={`${button.label}-${index}`}
+                        onClick={() =>
+                            handleButtonsChange({ buttons, setButtons })(button.label)
+                        }
+                    >
+                        {button.label}
+                    </button>
+                ))}
+            </>
+        )
+    }
+    
+    return (
+        
+        <div>
+            <p>Kingdoms rich in ancient grandeur, halls carved into the roots of mountains, the echoing of picks and hammers in deep mines and blazing forges, a commitment to clan and tradition, and a burning hatred of goblins and orcs—these common threads unite all dwarves.</p>
+            <h2>Dwarf Traits</h2>
+            <img class="raceImg" src={Picture} alt="Drawf"/>
+            <p>Your dwarf character has an assortment of inborn abilities, part and parcel of dwarven nature.</p>
+
+            <p> Choose a subrace for additional traits, appended at the bottom of the list:</p>
+            <Specialbuton {...{buttons,setButtons,handleButtonsChange}} />
+            {buttons[0].value && <div><p>As a hill dwarf, you have keen senses, deep intuition, and remarkable resilience.</p></div>}
+            {buttons[1].value && <div><p>As a mountain dwarf, you're strong and hardy, accustomed to a difficult life in rugged terrain</p></div>}
+
+
+            <p><b>Ability Score Increase:</b> Your 
+            <div class="tooltip">&nbsp;Constitution&nbsp;
+                <span class="tooltiptext"><Constitution/></span>
+            </div>
+            score increases by 2.</p>
+            <p><b>Age:</b> Dwarves mature at the same rate as humans, but they're considered young until they reach the ages of 50. On average, they live about 350 years.</p>
+            <p><b>Size:</b> Dwarves stand between 4 and 5 feet tall and average about 150 pounds. Your size is medium.</p>
+            <p><b>Speed:</b> Your base walking speed is 
+            <div class="tooltip">&nbsp;25 feet.
+                <span class="tooltiptext">This is how far you can move in a single round of combat without using a dash action.</span>
+            </div>
+            &nbsp;Your speed is not reduced by wearing heavy armor.</p>
+            <p><b>Darkvision:</b> Accustomed to life underground, you have superior vision in the dark and dim conditions.  You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light.  You can't discern color in darkness, only shades of gray.</p>
+            <p><b>Dwarven Resilience:</b> You have 
+            <div class="tooltip">&nbsp;advantage&nbsp;
+                <span class="tooltiptext">Advantage means you can make a roll twice and take the higher of the two rolls, increasing your chance for success or a critical roll.</span>
+            </div>            
+            on saving throws against poison, and you have resistance against poison damage.</p>
+            <p><b>Dwarven Combat Training:</b> You have 
+            <div class="tooltip">&nbsp;proficiency&nbsp;
+                <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+            </div>  
+            with the battleaxe, handaxe, light hammer, and warhammer.</p>
+            <p><b>Tool Proficiency:</b> You gain 
+            <div class="tooltip">&nbsp;proficiency&nbsp;
+                <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+            </div> 
+            with the artisan's tools of your choice, smith's tools, brewer's supplies, or mason's tools.</p>
+            <p><b>Stonecunning:</b> Whenever you make a History check related to the origin of stonework, you are considered proficient in the History skill and add double your 
+            <div class="tooltip">&nbsp;proficiency&nbsp;
+                <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+            </div> 
+            bonus to the check.</p>
+            <p><b>Languages:</b> You can speak, read, and write Common and Dwarvish.  Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak.</p>
+            
+            
+            {buttons[0].value && <div>
+                <h2> Additional Hill Dwarf Traits</h2>
+                <p><b>Ability Score Increase:</b> Your
+                <div class="tooltip">&nbsp;Wisdom&nbsp;
+                <span class="tooltiptext"><Wisdom/></span>
+                </div>
+                score increases by 1.</p>
+                <p><b>Dwarven Toughness:</b> Your hit point maximum increases by 1, and it increases by 1 every time you gain a level.</p>
+                </div>}
+            {buttons[1].value && <div>
+                <h2> Additional Mountain Dwarf Traits</h2>
+                <p><b>Ability Score Increase:</b> Your
+                <div class="tooltip">&nbsp;Strength&nbsp;
+                <span class="tooltiptext"><Strength/></span>
+                </div> 
+                score increases by 2.</p>
+                <p><b>Dwarven Armor Training:</b> You have 
+                <div class="tooltip">&nbsp;proficiency&nbsp;
+                    <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+                </div>  
+                with light and medium armor.</p>
+                </div>}
+        </div>
+    )        
+
+
+}
+
+
+export default DwarfRace;
---
+++ src/components/races/Gnome.js
@@ -0,0 +1,146 @@
+import React from 'react'
+import Picture from '../pics/gnome.png'
+import {useState} from 'react';
+import {Charisma, Constitution, Dexterity, Intelligence, Wisdom} from '../keywords/AbilityScores';
+
+function GnomeRace() {
+
+    const [buttons, setButtons] = useState([
+        { class:"buttonsubrace", label: "Forest Gnome", value: false },
+        { class:"buttonsubrace", label: "Rock Gnome", value: false }
+        
+    ]);
+
+    const handleButtonsChange = ({
+        buttons,
+        setButtons,
+        handleButtonsChange
+    }) => label => {
+        const newButtonsState = buttons.map(button => {
+            if (button.label === label) {
+                return (button = { class: "buttonsubrace_clicked", label: button.label, value: true});
+            }
+            return {
+                class: "buttonsubrace",
+                label: button.label,
+                value: false
+            };
+        });
+        setButtons(newButtonsState)
+    };
+       
+    const Specialbuton = ({ buttons, setButtons, handleButtonsChange }) => {
+        return (
+            <>
+                {buttons.map((button,index) => (
+                    <button class={`${button.class}`}
+                        key={`${button.label}-${index}`}
+                        onClick={() =>
+                            handleButtonsChange({ buttons, setButtons })(button.label)
+                        }
+                    >
+                        {button.label}
+                    </button>
+                ))}
+            </>
+        )
+    }
+
+    return (
+        <div>
+            <p>A constant hum of busy activity pervades the warrens and neighborhoods where gnomes form their close-knit communities. Louder sounds punctuate the hum: a crunch of grinding gears here, a minor explosion there, a yelp of surprise or triumph, and especially bursts of laughter. Gnomes take delight in life, enjoying every moment of invention, exploration, investigation, creation, and play.</p>
+            <h2>Gnome Traits</h2>
+            <img class="raceImg" src={Picture} alt="Gnome"/>
+
+            <p>Your gnome character has certain characteristics in common with all other gnomes.</p>
+
+            <p> Choose a subrace for additional traits, appended at the bottom of the list:</p>
+            <Specialbuton {...{buttons,setButtons,handleButtonsChange}} />
+            {buttons[0].value && <div><p>As a forest gnome, you have a natural knack for illusion and inherent quickness and stealth. </p></div>}
+            {buttons[1].value && <div><p>As a rock gnome, you have a natural inventiveness and hardiness beyond that of other gnomes.</p></div>}
+            
+
+            <p><b>Ability Score Increase:</b> Your 
+            <div class="tooltip">&nbsp;Intelligence&nbsp;
+                <span class="tooltiptext"><Intelligence/></span>
+            </div>
+            score increases by 2.</p>
+            <p><b>Age:</b> Gnomes mature at the same rate humans do, and most are expected to settle down into an adult life by around age 40. They can live 350 to almost 500 years.</p>
+            <p><b>Size:</b> Gnomes are between 3 and 4 feet tall and average about 40 pounds. Your size is Small.</p>
+            <p><b>Speed:</b> Your base walking speed is 
+            <div class="tooltip">&nbsp;25 feet.
+                <span class="tooltiptext">This is how far you can move in a single round of combat without using a dash action.</span>
+            </div>
+            </p>
+            <p><b>Darkvision:</b> Accustomed to life underground, you have superior vision in the dark and dim conditions.  You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light.  You can't discern color in darkness, only shades of gray.</p>
+            <p><b>Gnome Cunning:</b> You have 
+            <div class="tooltip">&nbsp;advantage&nbsp;
+                <span class="tooltiptext">Advantage means you can make a roll twice and take the higher of the two rolls, increasing your chance for success or a critical roll.</span>
+            </div>            
+            on all 
+            <div class="tooltip">&nbsp;Intelligence
+                <span class="tooltiptext"><Intelligence/></span>
+            </div>
+            ,
+            <div class="tooltip">&nbsp;Wisdom
+                <span class="tooltiptext"><Wisdom/></span>
+            </div>
+            , and 
+            <div class="tooltip">&nbsp;Charisma&nbsp;
+                <span class="tooltiptext"><Charisma/></span>
+            </div>   
+            saving throws against magic.</p>
+            <p><b>Languages:</b>You can speak, read, and write Common and Gnomish. The Gnomish language, which uses the Dwarvish script, is renowned for its technical treatises and its catalogs of knowledge about the natural world.</p>
+        
+            {buttons[0].value && <div>
+                <h2> Additional Forest Gnome Traits</h2>
+                <p><b>Ability Score Increase:</b> Your
+                <div class="tooltip">&nbsp;Dexterity&nbsp;
+                    <span class="tooltiptext"><Dexterity/></span>
+                </div>
+                score increases by 1.</p>
+                <p><b>Natural Illusionist:</b> You know the 
+                <div class="tooltip">&nbsp;minor illusion
+                    <span class="tooltiptext">You create a sound or an image of an object within range that lasts for the duration. The illusion also ends if you dismiss it as an action or cast this spell again.</span>
+                </div>
+                <div class="tooltip">&nbsp;cantrip
+                    <span class="tooltiptext">A cantrip is a spell that can be cast at will, without using a spell slot and without being prepared in advance. Repeated practice has fixed the spell in the caster's mind and infused the caster with the magic needed to produce the effect over and over. A cantrip's spell level is 0.</span>
+                </div> 
+                . 
+                <div class="tooltip">&nbsp;Intelligence&nbsp;
+                    <span class="tooltiptext"><Intelligence/></span>
+                </div>
+                is your spellcasting ability for it. </p>
+                <p><b>Speak with Small Beasts:</b> Through sounds and gestures, you can communicate simple ideas with Small or smaller beasts. Forest gnomes love animals and often keep squirrels, badgers, rabbits, moles, woodpeckers, and other creatures as beloved pets.</p>
+
+            </div>}
+
+            {buttons[1].value && <div>
+                <h2> Additional Rock Gnome Traits</h2>
+                <p><b>Ability Score Increase:</b> Your
+                <div class="tooltip">&nbsp;Constitution&nbsp;
+                    <span class="tooltiptext"><Constitution/></span>
+                </div>
+                score increases by 1.</p>
+                <p><b>Artificer's Lore:</b> Whenever you make a History check related to magic items, alchemical objects, or technological devices, you can add twice your 
+                <div class="tooltip">&nbsp;proficiency&nbsp;
+                    <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+                </div>  
+                bonus, instead of any proficiency bonus you normally apply. </p>
+                <p><b>Tinker:</b> You have 
+                <div class="tooltip">&nbsp;proficiency&nbsp;
+                    <span class="tooltiptext">A bonus that you can add to a roll to increase your chance of success, due to your "proficiency" in the skill.</span>
+                </div>  
+                with artisan's tools (tinker's tools). Using those tools, you can spend 1 hour and 10 gold pieces worth of materials to construct a Tiny clocker device, either a Clockwork Toy, a Fire Starter, or a Music Box.</p>
+                
+               
+            </div>}
+
+        </div>
+    )        
+
+
+}
+
+
+export default GnomeRace;
